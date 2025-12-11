024f1b551e20d93ee035f7580569347a6d34b505
Balthazar234 <hussainhassanat08@gmail.com>
2025-12-11 15:23:23 +0500

    initial commit

--- main.tf
+++ main.tf
@@ -2,19 +2,62 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-00f46ccd1cbfb363e"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+# Fetch default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Fetch all subnets in that VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Fetch latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
-              nohup busybox httpd -f -p 80 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  tags = {
-    Name = "ExampleInstance"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+  min_size             = 2
+  max_size             = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-example-asg"
+    propagate_at_launch = true
   }
 }
 
@@ -22,9 +65,15 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-sg"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+variable "server_port" {
+  description = "The port on which the server will listen"
+  type        = number
+  default     = 8080
 }
