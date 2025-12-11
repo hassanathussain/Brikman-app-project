dd29b94b67013648fc36951ddd56fd58f4cff4fc
Balthazar234 <hussainhassanat08@gmail.com>
2025-12-11 12:43:27 +0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-00f46ccd1cbfb363e"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
