83aef88d2b71d7c2c769a3d4943af38c7db74d8e
Balthazar234 <hussainhassanat08@gmail.com>
2025-12-11 16:39:04 +0500

    initial commit

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
+########################################
+# Provider
+########################################
 provider "aws" {
   region = "us-west-2"
 }
 
-# Fetch default VPC
+########################################
+# Data Sources
+########################################
+
+# Default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Fetch all subnets in that VPC
+# Subnets in the default VPC
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
@@ -15,7 +22,7 @@ data "aws_subnets" "default" {
   }
 }
 
-# Fetch latest Ubuntu AMI
+# Latest Ubuntu 20.04 AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -29,30 +36,153 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"]  # Canonical
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id        = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+########################################
+# Security Groups
+########################################
 
-  user_data = <<-EOF
+# ALB security group
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2/ASG instance security group
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance-sg"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+########################################
+# Load Balancer
+########################################
+
+resource "aws_lb" "example" {
+  name               = "example-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404 Not Found"
+      status_code  = 404
+    }
+  }
+}
+
+########################################
+# Target Group
+########################################
+
+resource "aws_lb_target_group" "asg_targets" {
+  name     = "example-asg-tg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Forward ALL requests to the ASG
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg_targets.arn
+  }
+}
+
+########################################
+# Launch Template (Replacement for Launch Configuration)
+########################################
+
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-lt-"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  user_data = base64encode(<<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  )
 
-  lifecycle {
-    create_before_destroy = true
+  network_interfaces {
+    security_groups = [aws_security_group.instance.id]
   }
 }
 
+########################################
+# Auto Scaling Group
+########################################
+
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-  min_size             = 2
-  max_size             = 10
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  target_group_arns   = [aws_lb_target_group.asg_targets.arn]
+  health_check_type   = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
 
   tag {
     key                 = "Name"
@@ -61,19 +191,17 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-sg"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+########################################
+# Variables & Outputs
+########################################
 
 variable "server_port" {
-  description = "The port on which the server will listen"
+  description = "Port application listens on"
   type        = number
   default     = 8080
+}
+
+output "alb_dns_name" {
+  description = "Public DNS name of the load balancer"
+  value       = aws_lb.example.dns_name
 }
